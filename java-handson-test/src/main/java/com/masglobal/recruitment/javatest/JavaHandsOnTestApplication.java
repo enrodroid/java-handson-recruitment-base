d4794ab787bffe725631ab9e9fdb89bc75ed1b45
Enrique F. Agudo <birliqui@gmail.com>
2019-03-08 14:01:44 -0500

    Initial configurations!

---
+++ java-handson-test/src/main/java/com/masglobal/recruitment/javatest/JavaHandsOnTestApplication.java
@@ -0,0 +1,13 @@
+package com.masglobal.recruitment.javatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHandsOnTestApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(JavaHandsOnTestApplication.class, args);
+  }
+  
+}
